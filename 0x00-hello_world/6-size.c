0ddf6e7b85784401982349d9b4f7234edad043c5
faraji-fuji <farajifuji@gmail.com>
2022-03-10 20:31:46 +0000

    Printing the size if variable types.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - print size of variable types
+  *
+  * Return: zero
+  */
+int main(void)
+{
+	printf("Size of a Char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long));
+	printf("Size of float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
