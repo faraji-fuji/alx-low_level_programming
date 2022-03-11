f9794db7c815a98a016bd574e1c9f33da08e644b
faraji-fuji <farajifuji@gmail.com>
2022-03-11 22:23:09 +0000

    Using conditions in C

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/**
+ * main - Print last digit of the random integer n; with different conditions
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+	int last = (n % 10);
+
+	if (last < 0)
+	{
+		last = (last * -1);
+	}
+	if (last > 5)
+	{
+		printf("Last digit of %d", n);
+		printf(" is %d", last);
+		printf(" and is greater than 5\n");
+	}
+	else if (last < 6 && last != 0)
+	{
+		printf("Last digit of %d", n);
+		printf(" is %d", last);
+		printf(" and is less than 6 and not 0\n");
+	}
+	else
+	{
+		printf("Last digit of %d", n);
+		printf(" is %d", last);
+		printf(" and is 0\n");
+	}
+	return (0);
+}
