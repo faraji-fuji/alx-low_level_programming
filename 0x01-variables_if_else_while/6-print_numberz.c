9b35c8e3744ee9f15483a27439bea5f1d83ba957
faraji-fuji <farajifuji@gmail.com>
2022-03-12 00:43:11 +0000

    Printing numbers with putchar, correction1

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,15 +10,13 @@ int main(void)
 {
 	int i;
 	int myChar;
-	char newLine;
 
 	myChar = '0';
-	newLine = '\n';
 	for (i = 0; i < 10; i++)
 	{
 		putchar(myChar);
 		myChar++;
 	}
-	putchar(newLine);
+	putchar('\n');
 	return (0);
 }
