cff2abfb84c695c6ed91999f0e8f9c264d82697e
faraji-fuji <farajifuji@gmail.com>
2022-03-12 01:23:33 +0000

    Combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,5 +8,22 @@
  */
 int main(void)
 {
+	int i;
+	int myChar;
+
+	myChar = '0';
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(myChar);
+		if (i == 9) 
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+		myChar++;
+	}
+	putchar ('\n');
 	return (0);
 }
