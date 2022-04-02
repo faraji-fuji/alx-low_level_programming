373709911f4c7eb7095b9cdc05f9091cece6f013
faraji-fuji <farajifuji@gmail.com>
2022-04-02 10:45:54 +0000

    add, correction

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,20 +20,21 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-
-	for (i = 1; i < argc; i++)
+	else
 	{
-		if (atoi(argv[i]) && atoi(argv[i]) >= 0)
+		for (i = 1; i < argc; i++)
 		{
-			sum += atoi(argv[i]);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
+			if (atoi(argv[i]) && atoi(argv[i]) >= 0)
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 	}
 	printf("%d\n", sum);
 	return (0);
-
 }
