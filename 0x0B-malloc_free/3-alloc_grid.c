cfe6f1747534d3bd778e280042c3e5e7cfe61cfb
faraji-fuji <farajifuji@gmail.com>
2022-04-05 12:30:12 +0000

    grid, correction 2

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,8 +26,9 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	for (j = 0; i < width; i++)
-		for (i = 0; j < height; j++)
+	for (i = 0; i < height; i++)
+		for (j = 0; i < width; j++)
 			arr[i][j] = 0;
+
 	return (&arr[0]);
 }
