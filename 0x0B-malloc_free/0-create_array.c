db54f235e90398d4eec0b1f5cec3380984322e39
faraji-fuji <farajifuji@gmail.com>
2022-04-05 06:48:29 +0000

    create an array, correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,9 +15,19 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
+	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	arr = malloc(sizeof(char) * size);
-	arr[0] = c;
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
 
 	return (&arr[0]);
 }
