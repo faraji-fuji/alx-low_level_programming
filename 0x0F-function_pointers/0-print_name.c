69567ce7124f32f789d0c78b682e815227287c10
faraji-fuji <farajifuji@gmail.com>
2022-04-13 09:45:24 +0000

    Print name, correction.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,15 @@
 /**
  * print_name -  A function that prints a name.
  *
+ * @name: Name to print.
+ * @f: Pointer to function.
+ *
  * Return: Zero.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	void (*func_to_call) (char *name);
+	void (*func_to_call)(char *name);
 
 	func_to_call = f;
 	func_to_call(name);
