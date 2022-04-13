c6cce0ecd05fde375f8ec026bddad076fbdaf77d
faraji-fuji <farajifuji@gmail.com>
2022-04-13 12:22:14 +0000

    int index, correction 8

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,20 +15,18 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int index, i;
+	int i;
 
 	if (size <= 0)
 		return (-1);
-	if (array && size && cmp)
+	if (array && cmp)
 	{
 		for (i = 0; i < size; i++)
 		{
-			index = cmp(array[i]);
-			if (index > 0)
-				return (index);
+			if (cmp(array[i]) != 0)
+				return (i);
+
 		}
 	}
-	else
-		return (-1);
-	return (index);
+	return (-1);
 }
