2454ab83029ddbb6f8df0dd84d7041f462312ec1
faraji-fuji <farajifuji@gmail.com>
2022-04-25 19:44:10 +0000

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,18 +2,14 @@
 #include <stdlib.h>
 
 /**
- * free_dog - free memory of dog
- * @d: pointer to dog
+ * free_dog - a function that frees dogs.
+ *
+ * @d: pointer to variable of type dog_t.
+ *
+ * Return: Nothing.
  */
 
 void free_dog(dog_t *d)
 {
-	if (d)
-	{
-		if (d->name)
-			free(d->name);
-		if (d->owner)
-			free(d->owner);
-		free(d);
-	}
+	free(d);
 }
