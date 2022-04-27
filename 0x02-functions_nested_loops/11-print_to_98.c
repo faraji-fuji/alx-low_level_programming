1321d376d00158290193b95838bc14101d1ec4fa
faraji-fuji <farajifuji@gmail.com>
2022-04-27 16:36:30 +0000

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -32,13 +32,3 @@ void print_to_98(int n)
 		printf("%d\n", n);
 	}
 }
-
-int main(void)
-{
-	print_to_98(0);
-	print_to_98(98);
-	print_to_98(111);
-	print_to_98(81);
-	print_to_98(-10);
-	return (0);
-}
