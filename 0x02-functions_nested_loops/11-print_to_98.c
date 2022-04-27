7b12ef9d062e4804085501dda55b8f8d18e56661
faraji-fuji <farajifuji@gmail.com>
2022-04-27 16:34:51 +0000

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,21 +13,32 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
 		}
-		printf("\n");
+		printf("%d\n", n);
 	}
 	else if (n == 98)
-		printf("%d", 98);
+		printf("%d\n", n);
 	else
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
 		}
+		printf("%d\n", n);
 	}
+}
+
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
 }
