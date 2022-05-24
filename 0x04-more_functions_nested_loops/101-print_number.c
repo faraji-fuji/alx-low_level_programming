9caa84c66907a6af2b6dca042a410a6f366403ca
faraji-fuji <farajifuji@gmail.com>
2022-05-24 09:01:29 +0000

    print number, correction

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,51 @@
 #include "main.h"
+
+/**
+ * get_decimal - get decimal places
+ * @n: integer to find decimal places for
+ *
+ * Return: decimal places
+ */
+int get_decimal(int n)
+{
+	int d = 1;
+
+	while (n > 0)
+	{
+		d *= 10;
+		n /= 10;
+	}
+
+	d = d / 10;
+	return (d);
+}
+
 /**
  * print_number - a function that prints an integer.
  * @n: integer to print
  *
  * Return: nothing
  */
-
 void print_number(int n)
 {
-	int d, number = n;
+	int d;
 
-	d = 1;
-	while (number % 10 > 0)
+	if (n == 0)
 	{
-		d *= 10;
-		number /= 10;
+		_putchar('0');
+		return;
 	}
-	d = d / 10;
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	d = get_decimal(n);
+
 	while (n > 0)
 	{
 		_putchar((n / d) + '0');
 		n %= d;
 		d /= 10;
 	}
-	_putchar('\n');
 }
